a56b3108953a0c6ec30bd59a0173722e1e8ef1c8
Md Farhad Islam <farhad.islam@outlook.com>
2022-10-05 13:47:02 +0100

    Added String sorting via Collections sort

--- src/islam/farhad/exercises/java8stream/StreamExamples2.java
+++ src/islam/farhad/exercises/java8stream/StreamExamples2.java
@@ -2,10 +2,7 @@ package islam.farhad.exercises.java8stream;
 
 import com.sun.source.tree.Tree;
 
-import java.util.Collections;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -21,12 +18,16 @@ public class StreamExamples2 {
         System.out.println("\nPrinting distinct elements");
         Stream.of(9, 11, 2, 2, 2, 3, 4, 4, 5, 6, 7).distinct().sorted((a, b) -> a > b ? 1 : -1).forEach(System.out::println);
 
-        String[] countries = {"UK", "", "UAE", "Uganda", "Turkiye", "Burma", "Canada", "Bahrain", "Vietnam", "Singapore", "Qatar","USA", "Thailand", "Vietnam", "Singapore", "Qatar"};
+        String[] countries = {"Azerbaizan", "Zimbabwe","UK", "", "UAE", "Uganda", "Turkiye", "Burma", "Canada", "Bahrain", "Vietnam", "Singapore", "Qatar","USA", "Thailand", "Vietnam", "Singapore", "Qatar"};
         System.out.println("Printing distinct elements");
         Stream.of(countries).distinct().forEach(System.out::println);
         Stream.of(countries).distinct().map(s->s.toUpperCase()).forEach(System.out::println);
         Set<String> setOfCountries = Stream.of(countries).filter(s -> !s.isEmpty()).collect(Collectors.toSet());
         System.out.println("Printing set of countries \n" + setOfCountries);
+
+        Collections.sort(Arrays.asList(countries));
+        Stream.of(countries).distinct().forEach(System.out::println);
+
 
     }
 }
