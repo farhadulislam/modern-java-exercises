2c7f4adde1f0e575204f7e2dee7088a9dbb3321f
Farhd Islam <farhad.islam@outlook.com>
2022-10-05 23:49:13 +0100

    added reduce examples from Devoxx

--- src/islam/farhad/exercises/java8streamCollectors/CollectorsByDevoxx.java
+++ src/islam/farhad/exercises/java8streamCollectors/CollectorsByDevoxx.java
@@ -1,4 +1,18 @@
 package islam.farhad.exercises.java8streamCollectors;
 
+import islam.farhad.exercises.java8stream.Person;
+
+import java.util.List;
+
+import static islam.farhad.exercises.java8stream.Person.getPeople;
+
 public class CollectorsByDevoxx {
+    public static void main(String[] args) {
+       List<Person> people =  Person.getPeople();
+
+        System.out.println(people.stream().map(Person :: getAge)
+               //.reduce(0, (subtotal, age)-> (subtotal + age)));
+                //.reduce(0,(subtotal, age)-> Integer.sum(subtotal + age))) ;
+                .reduce(0, Integer::sum)); // Reduces verbosity.
+    }
 }
