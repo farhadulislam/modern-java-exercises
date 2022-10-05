c9171c08e8642500396532a27670522ea1a8be28
Md Farhad Islam <farhad.islam@outlook.com>
2022-10-05 15:51:21 +0100

    Grouping by age using 3 args overloaded Collectors.groupingBy Method in sorted order

--- src/islam/farhad/exercises/javaFundamentals/Employee.java
+++ src/islam/farhad/exercises/javaFundamentals/Employee.java
@@ -1,13 +1,24 @@
 package islam.farhad.exercises.javaFundamentals;
 
+import java.util.Objects;
+
 public class Employee {
     private int id;
     private String name;
     private String dept;
     private long salary;
+    private int age;
 
-    public Employee(int id, String name, String dept, long salary) {
+    public Employee(int id, String name, String dept, long salary){
         this.id = id;
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+    }
+
+    public Employee(int id,int age, String name, String dept, long salary) {
+        this.id = id;
+        this.age = age;
         this.name = name;
         this.dept = dept;
         this.salary = salary;
@@ -35,6 +46,14 @@ public class Employee {
         return salary;
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     @Override
     public String toString() {
         return "Employee{" +
@@ -43,5 +62,16 @@ public class Employee {
                 ", dept='" + dept + '\'' +
                 ", salary=" + salary +
                 '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee employee)) return false;
+        return Objects.equals(name, employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
--- src/islam/farhad/exercises/javaFundamentals/EmployeeDB.java
+++ src/islam/farhad/exercises/javaFundamentals/EmployeeDB.java
@@ -15,4 +15,19 @@ public class EmployeeDB {
 
         );
     }
+
+    public static List<Employee> getEmployeeList2 (){
+
+        return Arrays.asList(
+                new Employee(1, 35,"Allan", "Civil", 54000),
+                new Employee(2, 34, "Robin", "Electrical", 23000),
+                new Employee(3, 41, "Mark", "Mechanical", 79000),
+                new Employee(4,66, "Tim", "Software", 232000),
+                new Employee(5, 35,"Raisul", "Civil", 44000),
+                new Employee(6, 34, "Bahar", "Electrical", 88000),
+                new Employee(7, 41, "Ali", "Mechanical", 99000),
+                new Employee(8,66, "Shuvo", "Software", 132000)
+
+        );
+    }
 }
