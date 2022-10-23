089dc877f8ca9de7d6f6d4fc08674310cc3a689d
Farhd Islam <farhad.islam@outlook.com>
2022-10-23 13:36:30 +0100

    Timing compute using parallelStream and multiple thread

--- src/islam/farhad/exercises/java8stream/DevoxxLambdaAndStreams.java
+++ src/islam/farhad/exercises/java8stream/DevoxxLambdaAndStreams.java
@@ -1,5 +1,9 @@
 package islam.farhad.exercises.java8stream;
 
+import islam.farhad.exercises.commonTools.TimeIt;
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,11 +20,52 @@ public class DevoxxLambdaAndStreams {
                 .forEach(System.out::println);
 
        int sum1 = integers.stream()
-                .reduce(0, (total, e) -> Integer.sum(total, e)); // method ref can only be used when order of paramaetrs passed remain the same.
+                //.reduce(0, (total, e) -> Integer.sum(total, e)); // method ref can only be used when order of paramaetrs passed remain the same.
+                .reduce(0, Integer::sum);// Used method ref here instead of above.
+        System.out.printf("Sum using reduce and method ref : %d\n", sum1);
+        System.out.println("String concat using method ref");
 
-        int sum2 = integers.stream()
-                .reduce(0, Integer::sum);
+        System.out.println(integers.stream()
+                .map(String::valueOf)
+                //.reduce("", (carry, str)-> carry.concat(str))); //Here, 1st param is a target and 2nd is an args.
+                .reduce("", String::concat));
 
-        System.out.printf("Sum1 : %d, Sum2 : %d ", sum1, sum2);
+        //Given a set of values, double the even numbers and then return the total
+
+        int result = 0;
+        for (int e : integers){
+            if (e%2==0){
+                result += e *2;
+            }
+        }
+        System.out.println("Imperative style " + result);
+
+        System.out.println("Functional style using stream : " +
+                integers.stream()
+                .filter( i-> i%2 ==0)
+                .map(i -> i * 2)
+                .reduce(0, Integer::sum));
+
+        System.out.println("Even better this way "+
+                integers.stream()
+                        .filter( i-> i%2 ==0)
+                        .mapToInt(i -> i * 2)
+                        .sum());
+
+
+        // Running it inside thread and taking advantage of parallel stream
+
+        TimeIt.code(()->
+                System.out.println(
+                // integers.stream()
+                integers.parallelStream()
+                .filter( n -> n % 2 == 0)
+                .mapToInt(DevoxxLambdaAndStreams::compute)
+                        .sum()));
+    }
+
+    public static int compute(int n){
+        try{Thread.sleep(1000);}catch(Exception ex){}
+        return n * 2;
     }
 }
